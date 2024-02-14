932a501a5418af405d7331d0a9a952855d149b67
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2024-02-14 17:29:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const handleMouseOver = () => {
+    document.querySelector("#no-btn").remove();
+
+    let myDiv = document.querySelector("body");
+    // creating button element
+    let button = document.createElement("button");
+    button.id = "no-btn";
+    button.style.position = "absolute";
+    const randomTop = Math.random() * 100;
+    const randomLeft = Math.random() * 100;
+    button.style.top = `${randomTop}%`;
+    button.style.left = `${randomLeft}%`;
+    button.classList.add("btn");
+    button.classList.add("btn-secondary");
+    let text = document.createTextNode("No");
+
+    // appending text to button
+    button.appendChild(text);
+    myDiv.appendChild(button);
+
+    button.addEventListener("mouseover", function () {
+      handleMouseOver();
+    });
+  };
+
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {isClicked ? (
+            <h1>Great!</h1>
+          ) : (
+            <>
+              <h1>Mary Jane</h1>
+              <h1>Will you be my valentine?</h1>
+            </>
+          )}
+          {!isClicked && (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "16px",
+                }}
+              >
+                <button
+                  class="btn btn-primary"
+                  onClick={() => {
+                    setIsClicked(true);
+                    document.querySelector("#no-btn").remove();
+                  }}
+                >
+                  Yes
+                </button>
+                <button
+                  id="no-btn"
+                  class="btn btn-secondary"
+                  onMouseOver={handleMouseOver}
+                >
+                  No
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </header>
     </div>
   );
